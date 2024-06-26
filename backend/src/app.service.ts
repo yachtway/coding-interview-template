94f4f2fafbe32129196286a3a79fd3fec4447bbf
Anh Mai <2263443+abmai@users.noreply.github.com>
2024-06-25 21:59:42 -0700

    Return latest message snippet

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -20,6 +20,23 @@ export class AppService {
   }
 
   async getChannels() {
-    return await this.prismaService.channel.findMany();
+    const channels = await this.prismaService.channel.findMany();
+    // Populate each channel with the most recent message
+    for (const channel of channels) {
+      const messages = await this.prismaService.message.findMany({
+        where: {
+          channelId: channel.id,
+        },
+        orderBy: { createdAt: 'desc' },
+        take: 1,
+      });
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      /* @ts-ignore */
+      channel.lastMessageSnippet = messages[0]?.content || '';
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      /* @ts-ignore */
+      channel.lastMessageCreatedAt = messages[0]?.createdAt || '';
+    }
+    return channels;
   }
 }
