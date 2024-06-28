a70fa94723f3d80cbcdd52cbf9aafbe1315b4d18
Anh Mai <2263443+abmai@users.noreply.github.com>
2024-06-27 21:58:36 -0700

    Remove extra endpoints

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,16 +5,6 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('message')
-  getHello() {
-    return this.appService.getHello();
-  }
-
-  @Get('messages')
-  getMessagesDefault() {
-    return this.appService.getMessages();
-  }
-
   @Get('messages/:channelId')
   getMessages(@Param('channelId') channelId: string) {
     return this.appService.getMessages(channelId);
